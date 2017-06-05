ba7f2e17b3bdc5ea02d65701c137623d19a7bbed
Anton Lejon <little.burrito@gmail.com>
2017-06-06 00:39:36 +0200

    Test 1

---
+++ index.php
@@ -0,0 +1,14 @@
+<pre>
+<?
+    echo exec( 'whoami' );
+    echo "\n";
+    echo exec( 'id' );
+    echo "\n";
+    echo exec( 'pwd' );
+    echo "\n";
+    echo exec( 'ls -al' );
+    echo exec( 'whoami > ./test.txt' );
+    echo "\n";
+    include( './test.txt' );
+?>
+</pre>
